35b34c17d437c934e5bf8e7f07c79fbd9d55cd08
nwmorgan21 <44788786+nwmorgan21@users.noreply.github.com>
2018-11-14 21:36:31 -0800

    Push 2

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-# This file was created by Nate Morgan
-# Sourced from Chris Bradfield from Kids can code
+# this file was created by nate morgan
+# thanks Chris Bradfield from Kids Can Code.
+# I've not got this on github...
 
-import pygame as pg 
+import pygame as pg
 import random
-from settings import *
+from setting import *
 from sprites import *
 
 class Game:
     def __init__(self):
-        # init game window
+        # init game window, try:
         pg.init()
         pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH,HEIGHT))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption("jumpy")
-        self.clock= pg.time.Clock()
-        self.running = True 
-        # init pygame
+        self.clock = pg.time.Clock()
+        self.running = True
+        # init pygame and create...
     def new(self):
         self.all_sprites = pg.sprite.Group()
-        self.player = Player()
-        self.all_sprites.add(self.player)
-        self.run
-        # create new player
-    def run(self):
+        self.all_platforms = pg.sprite.Group()
+
+        p1 = Platform(0, HEIGHT - 40, WIDTH, 40)
+        p2 = Platform(65, HEIGHT - 150, WIDTH - 300, 40)
+
+        self.player1 = Player(self)
+        #self.player2 = Player(WHITE)
+
+        self.all_sprites.add(self.player1)
+        #self.all_sprites.add(self.player2)
+
+        self.all_sprites.add(p1)
+        self.all_platforms.add(p1)
+        self.all_sprites.add(p2)
+        self.all_platforms.add(p2)
+
+        self.run()
+        # create new player object
+    def run(self): 
         self.playing = True
         # game loop
         while self.playing:
@@ -30,22 +45,34 @@ class Game:
             self.events()
             self.update()
             self.draw()
+
     def update(self):
         self.all_sprites.update()
-        # update data
+        hits = pg.sprite.spritecollide(self.player1, self.all_platforms, False)
+        print(hits)
+
+        if hits:
+            self.player1.pos.y = hits[0].rect.top + 1
+            self.player1.vel.y = 0
+        #self.player2.update(pg.K_a,pg.K_d,pg.K_w)
+        # update things
     def events(self):
         
-        # listening for inputs 
+        # listening for events
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
-            
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player1.jump()
+
+
     def draw(self):
         self.screen.fill(REDDISH)
         self.all_sprites.draw(self.screen)
-        # display 
+        # double buffer
         pg.display.flip()
 
     def show_start_screen(self):
@@ -62,6 +89,3 @@ while g.running:
     g.show_go_screen()
 
 pg.quit()
-
-
-
--- sprites.py
+++ sprites.py
@@ -1,25 +1,61 @@
 import pygame as pg 
 from pygame.sprite import Sprite
 import random
-from settings import *
+from setting import *
+
+vec = pg.math.Vector2
 
 class Player(Sprite):
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
+        self.game = game
         self.image = pg.Surface((30,40))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.vx = 0
-        self.vy = 0
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        
     def update(self):
-        self.vx = 0
+        self.acc = vec(0,player_grav)
+
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
-            self.vx = -5
+            self.acc.x = -player_acc
         if keys[pg.K_RIGHT]:
-            self.vx = 5
-        
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+            self.acc.x = player_acc
 
+        #Player Friction
+        self.acc.x += self.vel.x * player_friction
+
+        #Equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+    def jump(self):
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.all_platforms, False)
+        self.rect.x += -1
+        if hits:
+            self.vel.y = -20
+
+
+
+class Platform(Sprite):
+    def __inti__(self, x, y, w, h):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+   
+
+#
